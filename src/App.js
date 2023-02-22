e0f6bed0fe2867fac430281f7670650096d58ca3
Edward Aliev <fred.bred.ea@yandex.ru>
2023-02-22 20:17:07 +0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { Center  } from '@chakra-ui/react'
 import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
-
 import logo from './logo.png';
 import ig from './icons/instagram.svg';
 import vk from './icons/vk.svg';
@@ -11,14 +10,21 @@ import fb from './icons/fb.svg';
 import yt from './icons/yt.svg';
 import spotify from './icons/spotify.svg';
 import am from './icons/am.svg';
- 
 import './App.css';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+
+
 function App() {
   const [events, setEvents] = useState([]); 
   const [preload, setPreload] = useState(true)
-  const apiURL = "https://admin.sati.show/api/events?sort=date";
-
-  useEffect(() => {      
+  const locate = useLocation();
+  const apiURL = `https://admin.sati.show/api/events?sort=date${(locate.pathname == '/en') ? '&locale=en' : ''}`;
+  console.log(locate.pathname);
+  useEffect(() => {
         
       getEvents();
       const onPageLoad = () => {
@@ -34,6 +40,10 @@ function App() {
       }
   }, []);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    getEvents();
+  }, [locate.pathname]);
  
   const getEvents = () => {
     axios.get(apiURL)
@@ -47,8 +57,10 @@ function App() {
     })
   }
 
-  function formatDate(newDate) {
-    const months = {
+  function formatDate(newDate,language) {
+    const months = {}
+
+    months.ru = {
       0: 'января',
       1: 'февраля',
       2: 'марта',
@@ -62,44 +74,77 @@ function App() {
       10: 'ноября',
       11: 'декабря',
     }
-    const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    months.en = {
+      0: 'January',
+      1: 'February',
+      2: 'March',
+      3: 'April',
+      4: 'May',
+      5: 'June',
+      6: 'July',
+      7: 'August',
+      8: 'September',
+      9: 'October',
+      10: 'November',
+      11: 'December',
+    }
+    console.log(months);
+    const days = {}
+    days.ru = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    days.en = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const d = newDate
     const year = d.getFullYear()
     const date = d.getUTCDate()
     const monthIndex = d.getMonth()
-    const monthName = months[d.getMonth()]
-    const dayName = days[d.getUTCDay()] // Thu
+    const monthName = (language == 'en' ? months.en[d.getMonth()] : months.ru[d.getMonth()])
+    const dayName = (language == 'en' ? days.en[d.getUTCDay()] : days.ru[d.getUTCDay()]) // Thu
     //const time = `${d.getHours()}:${(d.getMinutes()<10 ?'0':'') + d.getMinutes()}`
     const formatted = `${dayName}, ${date} ${monthName}`
     return formatted.toString()
   }
 
-  return (
-    <div className="App">
+  const Page = ({language}) => {
+    console.log(language);
+    return (
       <div className="content">
         { preload &&
           <Center w='100vw' h='100vh' position='absolute' zIndex='10000' background='#0F1120'>
             <CircularProgress isIndeterminate color='#E64BB6' size='150px' thickness='4px' />
           </Center>
         } 
+        <div className="internationalization">
+          <div className="containerDefault">
+            <Link to="/" className={'internationalization__locale'+(locate.pathname == '/' ? ' active' : '')}>RU</Link>
+            <Link to="/en" className={'internationalization__locale'+(locate.pathname == '/en' ? ' active' : '')}>EN</Link>
+          </div>
+        </div>
         <div className="logo"><img src={logo} /></div>
         <div className="events">
           {events.map((event, key)=> (
 
             <div className="events__event" key={key}>
               <div className="event__info">
-                <div className="event__info_date">{formatDate(new Date(event.attributes.date))+(event.attributes.time ? ' '+event.attributes.time : '')}</div>
+                <div className="event__info_date">{formatDate(new Date(event.attributes.date),language)+(event.attributes.time ? ' '+event.attributes.time : '')}</div>
                 <div className="event__info_title">{event.attributes.title}</div>
                 <div className="event__info_city">{event.attributes.city}{(event.attributes.place ? ', '+event.attributes.place : '')}</div>
               </div>
               <div className="event__actions">
-                <a className={"btn_buyTickets"+(!event.attributes.buttonActive ? " btn_buyTickets_disabled" : "")} href={(event.attributes.buttonActive ? event.attributes.link : "")} onClick={(e) => !event.attributes.buttonActive ? e.preventDefault() : ""}>Купить билеты</a>
+                <a className={"btn_buyTickets"+(!event.attributes.buttonActive ? " btn_buyTickets_disabled" : "")} href={(event.attributes.buttonActive ? event.attributes.link : "")} onClick={(e) => !event.attributes.buttonActive ? e.preventDefault() : ""}>{(language == 'en' ? 'Buy tickets' : 'Купить билеты')}</a>
               </div>
               {/* <div className="line"></div> */}
             </div>
           ))}
         </div>
       </div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Page language='ru'/>} />
+        <Route path="/en" element={<Page language='en'/>} />
+      </Routes>
       <div className="footer">
         <div className="socialItems">
           <div className="socialItems__item">
